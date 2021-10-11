c99a5378efc584a6e23ae9e7cd38a5ff9fb949df
连晓磊 <lianxiaolei@lixiang.com>
2021-10-11 14:29:24 +0800

    修改Trainer妆容CV与非CV

--- core/trainer.py
+++ core/trainer.py
@@ -62,25 +62,11 @@ class FinetuneTrainer:
         epoch = self.epoch
         print('数据样例')
         print(data.head())
-        gkf = StratifiedKFold(n_splits=kfold).split(X=data['input_ids'].fillna(0),
-                                                    y=np.array(list(range(len(data)))) // 100)
-
-        for fold, (train_idx, valid_idx) in enumerate(gkf):
-            cudnn.benchmark = True
-            print('=' * 40, 'fold: {}'.format(fold), '=' * 40)
-
-            src = torch.LongTensor(data.iloc[train_idx]['input_ids'].values.tolist())
-            seg = torch.LongTensor(data.iloc[train_idx]['segment_ids'].values.tolist())
-            mask = torch.LongTensor(data.iloc[train_idx]['input_mask'].values.tolist())
-            tgt = torch.LongTensor(data.iloc[train_idx]['label'].values.tolist())
-
-            src_v = torch.LongTensor(data.iloc[valid_idx]['input_ids'].values.tolist())
-            seg_v = torch.LongTensor(data.iloc[valid_idx]['segment_ids'].values.tolist())
-            mask_v = torch.LongTensor(data.iloc[valid_idx]['input_mask'].values.tolist())
-            tgt_v = torch.LongTensor(data.iloc[valid_idx]['label'].values.tolist())
-
-            # 释放显存占用
-            torch.cuda.empty_cache()
+        if kfold <= 1:
+            src = torch.LongTensor(data['input_ids'].values.tolist())
+            seg = torch.LongTensor(data['segment_ids'].values.tolist())
+            mask = torch.LongTensor(data['input_mask'].values.tolist())
+            tgt = torch.LongTensor(data['label'].values.tolist())
 
             train_num = len(src)
             train_steps = int(train_num * epoch / batch_size) + 1
@@ -139,7 +125,6 @@ class FinetuneTrainer:
                         pgd.restore()
 
                     optimizer.step()
-
                     scheduler.step()
 
                     global_steps += 1
@@ -152,63 +137,162 @@ class FinetuneTrainer:
                         acc = acc.type(torch.float)
                         acc = torch.sum(acc * mask_batch)
                         acc = acc / torch.sum(mask_batch)
-                    bar.set_description('step:{} acc:{} loss:{} lr:{}'.format(
-                        global_steps, round(acc.item(), 4), round(loss.item(), 4), round(scheduler.get_lr()[0], 7)))
+                    bar.set_description('step:{} loss:{} lr:{}'.format(
+                        global_steps, round(loss.item(), 4), round(scheduler.get_lr()[0], 7)))
 
-                # 每个epoch结束进行验证集评估并保存模型
-                labels = None
-                outputs = None
-                for k, (src_batch, tgt_batch, seg_batch, mask_batch) \
-                        in enumerate(batch_loader(batch_size, src_v, tgt_v, seg_v, mask_v)):
+            model_save_path = os.path.join(output_path, 'finetune_model')
+            model_to_save = model.module if hasattr(model, 'module') else model
 
-                    src_batch = src_batch.to(device)
-                    tgt_batch = tgt_batch.to(device)
-                    seg_batch = seg_batch.to(device)
-                    mask_batch = mask_batch.to(device)
+            model_to_save.save_pretrained(model_save_path)
 
-                    output = model(input_ids=src_batch, labels=tgt_batch,
-                                   token_type_ids=seg_batch, attention_mask=mask_batch)
+        else:
+            gkf = StratifiedKFold(n_splits=kfold).split(X=data['input_ids'].fillna(0),
+                                                        y=np.array(list(range(len(data)))) // 100)
 
-                    output = output[1].cpu().detach().numpy()
-                    tgt_batch = tgt_batch.cpu().detach().numpy()
+            for fold, (train_idx, valid_idx) in enumerate(gkf):
+                cudnn.benchmark = True
+                print('=' * 40, 'fold: {}'.format(fold), '=' * 40)
 
-                    output = np.argmax(output, axis=-1)
-                    if labels is None:
-                        labels = tgt_batch
-                        outputs = output
-                    else:
-                        labels = np.concatenate([labels, tgt_batch], axis=0)
-                        outputs = np.concatenate([outputs, output], axis=0)
+                src = torch.LongTensor(data.iloc[train_idx]['input_ids'].values.tolist())
+                seg = torch.LongTensor(data.iloc[train_idx]['segment_ids'].values.tolist())
+                mask = torch.LongTensor(data.iloc[train_idx]['input_mask'].values.tolist())
+                tgt = torch.LongTensor(data.iloc[train_idx]['label'].values.tolist())
 
-                labels = np.reshape(labels, [-1])
-                outputs = np.reshape(outputs, [-1])
+                src_v = torch.LongTensor(data.iloc[valid_idx]['input_ids'].values.tolist())
+                seg_v = torch.LongTensor(data.iloc[valid_idx]['segment_ids'].values.tolist())
+                mask_v = torch.LongTensor(data.iloc[valid_idx]['input_mask'].values.tolist())
+                tgt_v = torch.LongTensor(data.iloc[valid_idx]['label'].values.tolist())
 
-                if model_type == 'tag':
-                    used_index = labels > 0
-                    labels = labels[used_index]
-                    outputs = outputs[used_index]
+                # 释放显存占用
+                torch.cuda.empty_cache()
 
-                met = multi_cls_metrics(labels, outputs, need_sparse=False, num_labels=num_labels)
-                acc = met['acc']
+                train_num = len(src)
+                train_steps = int(train_num * epoch / batch_size) + 1
 
-                table = PrettyTable(['global_steps',
-                                     'loss',
-                                     'lr',
-                                     'acc'])
-                table.add_row([global_steps + 1,
-                               total_loss / (global_steps + 1),
-                               scheduler.get_lr()[0],
-                               round(acc, 4)])
-                print(table)
+                model = build_model(ptm_name, num_labels, type=model_type)
+                optimizer, scheduler = build_optimizer(model, train_steps, learning_rate=learning_rate,
+                                                       weight_decay=weight_decay, warmup_ratio=warmup_ratio)
+                model.to(device)
 
-                if global_acc < acc:
-                    output_path_ = output_path + f'_fold{fold}'
-                    if not os.path.exists(output_path_):
-                        os.mkdir(output_path_)
-                    model_save_path = os.path.join(output_path_, 'finetune_model')
-                    model_to_save = model.module if hasattr(model, 'module') else model
+                total_loss = 0.0
+                global_steps = 0
+                global_acc = 0
+                bar = tqdm(range(1, epoch + 1))
+                for e in bar:
 
-                    model_to_save.save_pretrained(model_save_path)
+                    for i, (src_batch, tgt_batch, seg_batch, mask_batch) \
+                            in enumerate(batch_loader(batch_size, src, tgt, seg, mask)):
+                        src_batch = src_batch.to(device)
+                        tgt_batch = tgt_batch.to(device)
+                        seg_batch = seg_batch.to(device)
+                        mask_batch = mask_batch.to(device)
+
+                        output = model(input_ids=src_batch, labels=tgt_batch,
+                                       token_type_ids=seg_batch, attention_mask=mask_batch)
+                        loss = output[0]
+
+                        optimizer.zero_grad()
+                        loss.backward()
+
+                        total_loss += loss.item()
+                        # cur_avg_loss += loss.item()
+
+                        if adv == 'fgm':
+                            fgm = FGM(model)
+                            fgm.attack()
+                            adv_loss = model(input_ids=src_batch, labels=tgt_batch,
+                                             token_type_ids=seg_batch, attention_mask=mask_batch)[0]
+                            adv_loss.backward()
+                            fgm.restore()
+
+                        if adv == 'pgd':
+                            pgd = PGD(model, **adv_params)
+                            pgd.backup_grad()
+
+                            K = adv_params['adv_k']
+                            for t in range(K):
+                                pgd.attack(is_first_attack=(t == 0))
+                                if t != K - 1:
+                                    model.zero_grad()  # 在K次攻击中，pgd里已经记录下了梯度，故model的梯度可直接设为0
+                                else:
+                                    pgd.restore_grad()  # 在K次攻击后，用pgd里的梯度重置model的梯度，完成K步梯度对抗
+
+                                adv_loss = model(input_ids=src_batch, labels=tgt_batch,
+                                                 token_type_ids=seg_batch, attention_mask=mask_batch)[0]
+                                adv_loss.backward()
+                            pgd.restore()
+
+                        optimizer.step()
+
+                        scheduler.step()
+
+                        global_steps += 1
+
+                        acc = torch.argmax(torch.softmax(output[1], dim=-1), dim=-1) == tgt_batch
+                        if model_type == 'cls':
+                            acc = acc.type(torch.float)
+                            acc = torch.mean(acc)
+                        else:
+                            acc = acc.type(torch.float)
+                            acc = torch.sum(acc * mask_batch)
+                            acc = acc / torch.sum(mask_batch)
+                        bar.set_description('step:{} acc:{} loss:{} lr:{}'.format(
+                            global_steps, round(acc.item(), 4), round(loss.item(), 4), round(scheduler.get_lr()[0], 7)))
+
+                    # 每个epoch结束进行验证集评估并保存模型
+                    labels = None
+                    outputs = None
+                    for k, (src_batch, tgt_batch, seg_batch, mask_batch) \
+                            in enumerate(batch_loader(batch_size, src_v, tgt_v, seg_v, mask_v)):
+
+                        src_batch = src_batch.to(device)
+                        tgt_batch = tgt_batch.to(device)
+                        seg_batch = seg_batch.to(device)
+                        mask_batch = mask_batch.to(device)
+
+                        output = model(input_ids=src_batch, labels=tgt_batch,
+                                       token_type_ids=seg_batch, attention_mask=mask_batch)
+
+                        output = output[1].cpu().detach().numpy()
+                        tgt_batch = tgt_batch.cpu().detach().numpy()
+
+                        output = np.argmax(output, axis=-1)
+                        if labels is None:
+                            labels = tgt_batch
+                            outputs = output
+                        else:
+                            labels = np.concatenate([labels, tgt_batch], axis=0)
+                            outputs = np.concatenate([outputs, output], axis=0)
+
+                    labels = np.reshape(labels, [-1])
+                    outputs = np.reshape(outputs, [-1])
+
+                    if model_type == 'tag':
+                        used_index = labels > 0
+                        labels = labels[used_index]
+                        outputs = outputs[used_index]
+
+                    met = multi_cls_metrics(labels, outputs, need_sparse=False, num_labels=num_labels)
+                    acc = met['acc']
+
+                    table = PrettyTable(['global_steps',
+                                         'loss',
+                                         'lr',
+                                         'acc'])
+                    table.add_row([global_steps + 1,
+                                   total_loss / (global_steps + 1),
+                                   scheduler.get_lr()[0],
+                                   round(acc, 4)])
+                    print(table)
+
+                    if global_acc < acc:
+                        output_path_ = output_path + f'_fold{fold}'
+                        if not os.path.exists(output_path_):
+                            os.mkdir(output_path_)
+                        model_save_path = os.path.join(output_path_, 'finetune_model')
+                        model_to_save = model.module if hasattr(model, 'module') else model
+
+                        model_to_save.save_pretrained(model_save_path)
 
 
 if __name__ == '__main__':
